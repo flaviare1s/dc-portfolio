97ea90139848a4285d1d8fa9824cec07db8d4f9a
Flávia Reis <flaviarster@gmail.com>
2023-10-20 11:47:38 -0300

    Fiz mais exercícios de JavaScript

--- main.js
+++ main.js
@@ -19,7 +19,84 @@ function quadrado() {
 
     console.log('O quadrado do número ' + numero + ' é ' + quadrado + '.')
 }
-quadrado()
+quadrado();
 */
 
+/*
+function main() {
+    const x = true
+    if (x) {
+        console.log('Hello, World!')
+    } else {
+        console.log('É falso!')
+    }
+}
+main();
+*/
 
+// Switch case conditions:
+/*
+function main(){
+    const x = 1;
+
+    switch (x) {
+        case 0:
+            console.log('x é 0');
+            break;
+        case 1:
+            console.log('x é 1');
+            break;
+        default:
+            console.log('x é um número diferente de 0 ou de 1');
+            break;
+    }
+}
+main();
+*/
+
+/*
+function main() {
+    const a = prompt('Digite um número!')
+    const b = prompt('Digite outro número!')
+    const c = prompt('Digite mais um número!')
+
+    console.log(Number(a) + Number(b));
+
+    if(a + b > c) {
+        console.log(`A soma de ${a} e ${b} é maior do que ${c}.`)
+    } else {
+        console.log(`A soma de ${a} e ${b} não é maior do que ${c}.`)
+    }
+}
+main();
+*/
+
+/*
+function main() {
+    const a = Number(prompt('Digite um número!'));
+    const b = Number(prompt('Digite outro número!'));
+    const c = a + b
+    const d = a / b
+   
+
+    if(a === b) {
+        console.log(c)
+    } else {
+        console.log(d)
+    } 
+    
+    if (c % 3 === 0 || d % 3 === 0) {
+        console.log('O resultado é divisível por 3.')
+    } else {
+        console.log('O resultado não é divisível por 3.')
+    }
+}
+main();
+*/
+
+function main() {
+    const nome = prompt('Digite um nome')
+    console.log(nome.toUpperCase())
+}
+    
+main();

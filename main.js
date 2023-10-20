6e46dad49c3cbc9e1383430693affc2e1bbafde8
Flávia Reis <flaviarster@gmail.com>
2023-10-20 10:39:04 -0300

    fiz exercícios para treinar JavaScript

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-import './style.css'
+import './style.css';
+
+/*
+function main() {
+    const nome = prompt('Qual o seu nome?');
+    const idade = prompt('qual a sua idade?');
+
+    const nomidade = 'O seu nome é ' + nome + ' e a sua idade é ' + idade + '.'
+
+    console.log(nomidade);
+}
+main();
+*/
+
+/*
+function quadrado() {
+    const numero = prompt('Digite um número')
+    const quadrado = numero ** 2
+
+    console.log('O quadrado do número ' + numero + ' é ' + quadrado + '.')
+}
+quadrado()
+*/
+
 

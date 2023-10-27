c6316a5ca0c7593646b03c70b4cc344c547de5fd
Flávia Reis <flaviarster@gmail.com>
2023-10-27 10:46:17 -0300

    Criei exercicios02.js

---
+++ exercicios02.js
@@ -0,0 +1,32 @@
+// FOR:
+
+/*
+function main() {
+     // for convencional
+    for (let x = 1; x <= 5; x++)
+    console.log(x)
+}
+main()
+*/
+
+/*
+function main() {
+    // for each
+    const array = ['a', 1, 'c']
+    for (const item of array) {
+        console.log(item)
+    }
+}
+main()
+*/
+
+/*
+function main() {
+    // for each
+    const array = ['a', 1, 'c']
+    for (const key in array) {
+        console.log(key)
+    }
+}
+main()
+*/

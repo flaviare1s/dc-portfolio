52bdea5564723e5403cb8edb2d58de9c3155e60b
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-27 13:06:25 -0300

    Atualizei main.js

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ function fatorial() {
 fatorial()
 */
 
+function fatorial() {
+    const n = Number(prompt('Digite um número inteiro!'))
+    let fat = 1
+    for(let c = n; c > 1; c--) {
+        fat *= c
+    }
+    return fat
+}
+console.log(fatorial())
+
 
 
 // Calcule a divisão de dois números fatoriais M e N:

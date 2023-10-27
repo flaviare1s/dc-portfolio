bdc851436464335d6bd0bba06e1ee893d90031aa
Flávia Reis <flaviarster@gmail.com>
2023-10-27 11:59:53 -0300

    Fiz mais exercícios

--- exercicios02.js
+++ exercicios02.js
@@ -30,3 +30,55 @@ function main() {
 }
 main()
 */
+/*
+function main() {
+    const array = ['carro', 18, 'vermelho', 2014, 'azul', 'modelo']
+    
+    for(key in array) {
+        const keyInt = Number(key)
+        if(keyInt % 2 !== 0) {
+            const item = array[key]
+            console.log(item)
+        }
+    }
+}
+main()
+*/
+
+// Escreva um programa que receba um numero N e some todos os numeros de 1 ate este numero N:
+/*
+function somaTudo(num) {
+    let soma = 0
+    for ( let i = 1; i <= num; i++) {
+        soma = soma += i
+    }
+    return soma
+}
+
+function main() {
+    let n = Number(prompt('Digite um número!'))
+    console.log(somaTudo(n))
+}
+main()
+*/
+
+// Escreva um programa que receba uma string, e me retorne quantas vezes a letra "a" aparece
+/*
+function main() {
+    let totA = 0
+    const palavra = prompt('Digite uma palavra!')
+
+    for(const letra of palavra) {
+        if (letra === 'a' || letra === 'A') {
+            totA++
+        }
+    }
+    console.log(`A palavra tem ${totA} letras A.`)
+}
+main()
+*/
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+// Fatorial
+/*
+function fatorial() {
+    const n = Number(prompt('Digite um número!'))
+    let x = 1
+
+    for (let c = n; c >= 1; c--) {
+        x = x * c
+    }
+    console.log(x)
+}
+fatorial()
+*/
+
+
+
+// Calcule a divisão de dois números fatoriais M e N:

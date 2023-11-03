1d4c1253f21a6154693ddb6ea7362bc6eb852b02
Flávia Reis <flaviarster@gmail.com>
2023-11-03 09:21:04 -0300

    Fiz mais exercicios

--- exercicios04.js
+++ exercicios04.js
@@ -1,13 +1,35 @@
 // map, filter e reduce:
 
+
+// map modifica os itens do array. 
+// Exercício com map:
 // Exercício 01:
 // Dado o array [2, 4, 6, 15, 7, 9, 11, 300], retorne um array com o quadrado dos itens:
 
+/*
 function main() {
     const array = [2, 4, 6, 15, 7, 9, 11, 300]
     const arrayAoQuadrado = array.map(function(item){
         return item * item
     })
+
     console.log(arrayAoQuadrado)
 } 
+main()
+*/
+
+// filter filtra o array e retorna apenas alguns itens.
+// Exercício com filter:
+
+// Exercício 02:
+
+// Dado o array [2, 4, 6, 15, 7, 9, 11, 300], retorne outro array somente com os números  menores do que 4 ou maiores do que 9:
+
+function main() {
+    const array = [2, 4, 6, 15, 7, 9, 11, 300]
+    const arrayFiltrado = array.filter((item) => {
+        return item < 4 || item > 9  
+     })
+    console.log(arrayFiltrado)
+}
 main()

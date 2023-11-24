9061cb9f68768694089e581548503e50e1d684c2
flaviarster@gmail.com <flvia9133@digitalcollegesul.local>
2023-11-24 11:00:22 -0300

    Adicionei contador

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import "./style.css";
 
+let count = 0
 
+function criarContador() {
+  const botaoDeIncremento = document.querySelector('#targetContadorButton')
+  const outputEl = document.querySelector('#targetContadorOutput')
+
+  botaoDeIncremento.addEventListener('mousedown', () => {
+    outputEl.innerHTML = 1
+  })
+  
+botaoDeIncremento.addEventListener('mouseup', () => {
+  outputEl.innerHTML = 0
+})
+}
+
+function main() {
+  criarContador()
+}
+
+main()
 
 
 

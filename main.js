2c0b30408eee20453a1f17910b5f8d7f91070731
Flávia Reis <flaviarster@gmail.com>
2023-11-29 11:35:40 -0300

    Tentei fazer o exercício de mudar a tela de cor quando clica com o mouse

--- main.js
+++ main.js
@@ -1,60 +1,31 @@
-import "./style.css";
-
-/* Função para criar um CONTADOR:
-let count = 0
-
-function criarContador() {
-  const botaoDeIncremento = document.querySelector('#targetContadorButton')
-  const outputEl = document.querySelector('#targetContadorOutput')
-
-  botaoDeIncremento.addEventListener('click', () => {
-    count++
-    outputEl.innerHTML = count
-  })
-}
-*/
-
-// Função que quando mantém o mouse pressionado sobre o botão mosta '1' e quando despressiona mostra '0':
-function clickDoMouse() {
-  const botaoDeIncremento = document.querySelector('#targetContadorButton')
-  const outputEl = document.querySelector('#targetContadorOutput')
-
-  botaoDeIncremento.addEventListener('mousedown', () => {
-    outputEl.innerHTML = 1
-  })
-
-  botaoDeIncremento.addEventListener('mouseup', () => {
-    outputEl.innerHTML = 0
-  })
-}
-
+import "./style.css"
 
 function mouseLocation() {
   const mostrarX = document.querySelector('#outputX')
   const mostrarY = document.querySelector('#outputY')
-
-  const tela = document.querySelector('#body')
+  const tela = document.querySelector('body')
 
   tela.addEventListener('mousemove', (event) => {
     const x = event.clientX
     const y = event.clientY
-    
+
     mostrarX.innerHTML = x
     mostrarY.innerHTML = y
+
+    tela.addEventListener('click', () => {
+      const mousePosition = (x + y) / 2
+      console.log(mousePosition)
+      tela.style.backgroundColor = `rgb(${mousePosition}, ${mousePosition}, ${mousePosition})`
+    })
   })
 }
 
-
-
-
 function main() {
-  // criarContador()
-  clickDoMouse()
-  mouseLocation()
+    mouseLocation()
 }
 
-main()
 
+main()
 
 
 

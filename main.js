756ce11b7f89d4419e2fe588ffb82c1c290ba094
Flávia Reis <flaviarster@gmail.com>
2023-12-01 11:53:55 -0300

    Comecei a desenvolver o burger menu

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import "./style.css"
 
+function mostrarMenu() {
+  const linkMenus = document.querySelectorAll('.link')
+  const burgerMenu = document.querySelector('.burger')
+
+  burgerMenu.addEventListener('click', () => {
+    for (const linkMenu of linkMenus) {
+      linkMenu.style.display = 'block'
+    } 
+  })
+}
+
+mostrarMenu()
+
+
+    
+
+
